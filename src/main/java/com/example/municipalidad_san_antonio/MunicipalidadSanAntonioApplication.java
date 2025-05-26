8350dfb65e9441d5dbad5c31e02ef62027daa944
Cristian <cr.bernalo@duocuc.cl>
2025-05-26 14:00:19 -0400

    iniciando proyecto

---
+++ src/main/java/com/example/municipalidad_san_antonio/MunicipalidadSanAntonioApplication.java
@@ -0,0 +1,13 @@
+package com.example.municipalidad_san_antonio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MunicipalidadSanAntonioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MunicipalidadSanAntonioApplication.class, args);
+    }
+
+}
