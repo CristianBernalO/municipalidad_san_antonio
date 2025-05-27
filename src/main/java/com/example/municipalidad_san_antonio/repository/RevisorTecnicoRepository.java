8a6c9a6b7f38addbecfbc145cbf6383ef4eb11c2
Dani1804 <da.lizama@duocuc.cl>
2025-05-27 16:15:25 -0400

    metodos agregados a revisorTecnico

--- src/main/java/com/example/municipalidad_san_antonio/repository/RevisorTecnicoRepository.java
+++ src/main/java/com/example/municipalidad_san_antonio/repository/RevisorTecnicoRepository.java
@@ -4,6 +4,11 @@ import com.example.municipalidad_san_antonio.model.RevisorTecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 @Repository
 public interface RevisorTecnicoRepository extends JpaRepository<RevisorTecnico, Integer> {
+    RevisorTecnico findByEmail(String email);
+    RevisorTecnico findById(int idRevisor);
+    List<RevisorTecnico> findByNombre(String nombre);
+
 }
--- src/main/java/com/example/municipalidad_san_antonio/service/RevisorTecnicoService.java
+++ src/main/java/com/example/municipalidad_san_antonio/service/RevisorTecnicoService.java
@@ -1,9 +1,12 @@
 package com.example.municipalidad_san_antonio.service;
 
+import com.example.municipalidad_san_antonio.model.RevisorTecnico;
 import com.example.municipalidad_san_antonio.repository.RevisorTecnicoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -11,4 +14,30 @@ public class RevisorTecnicoService {
 
     @Autowired
     private RevisorTecnicoRepository revisorTecnicoRepository;
+
+    //Traer todos los revisores
+    public List<RevisorTecnico> findAll() {
+        return revisorTecnicoRepository.findAll();
+    }
+    // traer por id
+    public RevisorTecnico findById(int idRevisor) {
+        return revisorTecnicoRepository.findById(idRevisor);
+    }
+
+    // Borrar por id
+    public void delete(Integer idRevisor) {
+        revisorTecnicoRepository.deleteById(idRevisor);
+    }
+    //Traer por nombre
+    public List<RevisorTecnico> findByNombre(String nombre) {
+        return revisorTecnicoRepository.findByNombre(nombre);
+    }
+    //Traer por correo
+    public RevisorTecnico findByEmail(String email) {
+        return revisorTecnicoRepository.findByEmail(email);
+    }
+    //Guardar un Revisor
+    public RevisorTecnico save(RevisorTecnico revisorTecnico) {
+        return revisorTecnicoRepository.save(revisorTecnico);
+    }
 }
